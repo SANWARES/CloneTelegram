cf0a28b77dd6a52614a18a5addc412da1fc8e03c
Alexander Karpuschenko <a.karpuschenko@ya.ru>
2021-01-08 23:43:36 +0700

    Lesson 48

--- app/src/main/java/com/example/clonetelegram/utilits/appPermission.kt
+++ app/src/main/java/com/example/clonetelegram/utilits/appPermission.kt
@@ -8,11 +8,16 @@ import androidx.core.content.ContextCompat
 
 const val READ_CONTACTS = Manifest.permission.READ_CONTACTS
 const val PERMISSION_REQUEST = 200
+const val RECORD_AUDIO = Manifest.permission.RECORD_AUDIO
 
 
 fun checkPermission(permission: String): Boolean {
     return if (Build.VERSION.SDK_INT >= 23
-        && ContextCompat.checkSelfPermission(APP_ACTIVITY, permission) != PackageManager.PERMISSION_GRANTED) {
+        && ContextCompat.checkSelfPermission(
+            APP_ACTIVITY,
+            permission
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
         ActivityCompat.requestPermissions(APP_ACTIVITY, arrayOf(permission), PERMISSION_REQUEST)
         false
     } else true
