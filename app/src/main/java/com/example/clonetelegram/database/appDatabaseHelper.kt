be1307ed72605f6a6e693f4936c6eb10e01cdb1e
Alexander Karpuschenko <a.karpuschenko@ya.ru>
2021-01-08 17:27:36 +0700

    Lesson 46

--- app/src/main/java/com/example/clonetelegram/database/appDatabaseHelper.kt
+++ app/src/main/java/com/example/clonetelegram/database/appDatabaseHelper.kt
@@ -6,6 +6,7 @@ import com.example.clonetelegram.models.CommonModel
 import com.example.clonetelegram.models.UserModel
 import com.example.clonetelegram.utilits.APP_ACTIVITY
 import com.example.clonetelegram.utilits.AppValueEventListener
+import com.example.clonetelegram.utilits.TYPE_MESSAGE_IMAGE
 import com.example.clonetelegram.utilits.showToast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -28,6 +29,7 @@ const val NODE_USERS = "users"
 const val NODE_USERNAMES = "usernames"
 const val NODE_PHONES = "phones"
 const val NODE_PHONES_CONTACTS = "phones_contacts"
+const val FOLDER_MESSAGE_IMAGE = "message_image"
 
 
 const val FOLDER_PROFILE_IMAGE = "profile_image"
@@ -43,6 +45,7 @@ const val CHILD_TEXT = "text"
 const val CHILD_TYPE = "type"
 const val CHILD_FROM = "from"
 const val CHILD_TIMESTAMP = "timeStamp"
+const val CHILD_IMAGE_URL = "imageUrl"
 
 
 fun initFirebase() {
@@ -183,4 +186,25 @@ fun setNameToDatabase(fullname: String) {
             APP_ACTIVITY.mAppDrawer.updateHeader()
             APP_ACTIVITY.supportFragmentManager.popBackStack()
         }.addOnFailureListener { showToast(it.message.toString()) }
+}
+
+fun sendMessageAsImage(receivingUserID: String, imageUrl: String, messageKey: String) {
+    val refDialogUser = "$NODE_MESSAGES/$CURRENT_UID/$receivingUserID"
+    val refDialogReceivingUser = "$NODE_MESSAGES/$receivingUserID/$CURRENT_UID"
+
+    val mapMessage = hashMapOf<String, Any>()
+    mapMessage[CHILD_FROM] = CURRENT_UID
+    mapMessage[CHILD_TYPE] = TYPE_MESSAGE_IMAGE
+    mapMessage[CHILD_ID] = messageKey
+    mapMessage[CHILD_TIMESTAMP] = ServerValue.TIMESTAMP
+    mapMessage[CHILD_IMAGE_URL] = imageUrl
+
+
+    val mapDialog = hashMapOf<String, Any>()
+    mapDialog["$refDialogUser/$messageKey"] = mapMessage
+    mapDialog["$refDialogReceivingUser/$messageKey"] = mapMessage
+
+    REF_DATABASE_ROOT
+        .updateChildren(mapDialog)
+        .addOnFailureListener { showToast(it.message.toString()) }
 }
--- app/src/main/java/com/example/clonetelegram/utilits/appConstants.kt
+++ app/src/main/java/com/example/clonetelegram/utilits/appConstants.kt
@@ -3,3 +3,5 @@ package com.example.clonetelegram.utilits
 import com.example.clonetelegram.MainActivity
 
 lateinit var APP_ACTIVITY: MainActivity
+const val TYPE_MESSAGE_IMAGE = "image"
+const val TYPE_MESSAGE_TEXT = "text"
